cfd48d15be155eaaa8d7a5be5aa036103ae7de97
Luis von der Eltz <luisvde@outlook.de>
2023-06-23 18:23:21 +0200

    Add project files.

---
+++ NotificationsPlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesktopNotifications.Apple;
+using DesktopNotifications.FreeDesktop;
+using DesktopNotifications.Windows;
+
+internal class TestClass
+{
+    private static void Main(string[] args)
+    {
+        var m = new WindowsNotificationManager();
+        var m1 = new FreeDesktopNotificationManager();
+        var m2 = new AppleNotificationManager();
+    }
+}
